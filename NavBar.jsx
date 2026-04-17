04ac8eb1608c05d180377354c33c1f3f2265c243
chethansidramkr <chethan.sidram.s.123@kalvium.community>
2026-04-17 13:57:45 +0530

    Create NavBar.jsx

---
+++ NavBar.jsx
@@ -0,0 +1,13 @@
+
+import { NavLink } from "react-router-dom";
+
+export default function NavBar({ count }) {
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/saved">
+        Saved {count > 0 && <span>({count})</span>}
+      </NavLink>
+    </nav>
+  );
+}
